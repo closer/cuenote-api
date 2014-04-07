2a145d1a9328187756d970ad1f16c80029d78ee4
Eido NABESHIMA <closer009@gmail.com>
2014-04-07 22:15:28 +0900

    connection

--- lib/cuenote/api/connection.rb
+++ lib/cuenote/api/connection.rb
@@ -1,6 +1,7 @@
 module Cuenote::Api
   class Connection
     def send(req)
+      uri = req.uri
       response = Net::HTTP.start uri.hostname, uri.port, use_ssl: (uri.scheme == 'https') do |http|
         http.request req
       end
--- lib/cuenote/api/version.rb
+++ lib/cuenote/api/version.rb
@@ -1,5 +1,5 @@
 module Cuenote
   module Api
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
